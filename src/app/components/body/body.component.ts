7883fb6576395c38120432742a7e653d2290391a
Rafaelenarvaez <100246546+Rafaelenarvaez@users.noreply.github.com>
2024-02-26 15:24:17 -0400

    new section aliados

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
+import { AliadosComponent } from '../aliados/aliados.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, AliadosComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss'
 })
